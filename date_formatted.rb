be96ab21d5cbebe6cddc85f7968d43bcb4e91ad9
Kyle Fredrickson <kyle.fredrickson@chicagobooth.edu>
2023-04-04 15:04:54 +0000

    Final

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,5 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2022, the calendar day is: 1, and the month is: 7."
+require"date"
+p "The year is: "+Date.today.year.to_s+", the calendar day is: "+Date.today.day.to_s+", and the month is: "+Date.today.month.to_s+"."
--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,8 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+require"date"
+rubyCreate=Date.new(1995,12,21)
+age=Date.today-rubyCreate
+age_i=age.to_i
+p "Ruby is "+age_i.to_s+" days old!"
